3b7c7ac8caa8a3e298384352f99f4b520b902162
Yu Ting Pan <86286128+uatueun@users.noreply.github.com>
2021-10-05 22:21:43 +0800

    基本設定

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
 import Vue from 'vue';
+// 載入 vue axios
+// import * as Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+// 外部套件往前 自寫套件往後
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
-  render: (h) => h(App),
+    render: (h) => h(App),
 }).$mount('#app');
